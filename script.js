b78e29da569f6763a56643f31065a97b91665c0e
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-06-04 12:20:11 -0400

    small addition to submit function - still not working

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ submit.addEventListener('click', (e) => {
     let title = document.getElementById("task-title").value;
     let details = document.getElementById("task-details").value;
     var cards = document.querySelectorAll('.project-card');
-    for (let i = 0; i < cards.length; i++) {
-        let innerTitle = cards[i].getElementsByClassName("card-title")[0]
-        let innerDetails = cards[i].getElementsByClassName("card-details")[0]
+    for (let i = 1; i < cards.length; i++) {
+        let innerTitle = cards[i].getElementsByClassName("task-title")[i]
+        let innerDetails = cards[i].getElementsByClassName("task-details")[i]
         if (innerDetails.innerHTML === 'Nothing here yet...') {
             innerTitle.html = title;
             innerDetails.innerHTML = details;
