332b37a59860e8b4627afb20ead1248136a80da3
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-06-04 12:16:56 -0400

    JS started but need to fix innerDetails function

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ final.addEventListener('click', (e) => {
     document.getElementById("add-msg").innerHTML = "";
     newCard.style.display = "flex";
 })
+
+let submit = document.getElementById("task-btn");
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    let title = document.getElementById("task-title").value;
+    let details = document.getElementById("task-details").value;
+    var cards = document.querySelectorAll('.project-card');
+    for (let i = 0; i < cards.length; i++) {
+        let innerTitle = cards[i].getElementsByClassName("card-title")[0]
+        let innerDetails = cards[i].getElementsByClassName("card-details")[0]
+        if (innerDetails.innerHTML === 'Nothing here yet...') {
+            innerTitle.html = title;
+            innerDetails.innerHTML = details;
+            break;
+            }
+    }
+})
