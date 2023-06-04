0f44127e38cb1eb1c5d880ada67df84277d78246
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-06-03 21:09:30 -0400

    new card js begun

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let final = document.getElementsByClassName("final-card")[0];
 let newCard = document.getElementById("new-card");
 final.addEventListener('click', (e) => {
-    document.getElementById("add-msg").innerHTML = ""
+    document.getElementById("add-msg").innerHTML = "";
     newCard.style.display = "block";
 })
