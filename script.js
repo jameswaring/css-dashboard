32af4eb6477a89bc44adc94645e33239532ad2c2
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-06-03 21:08:47 -0400

    left menu items starte

---
+++ script.js
@@ -0,0 +1,6 @@
+let final = document.getElementsByClassName("final-card")[0];
+let newCard = document.getElementById("new-card");
+final.addEventListener('click', (e) => {
+    document.getElementById("add-msg").innerHTML = ""
+    newCard.style.display = "block";
+})
