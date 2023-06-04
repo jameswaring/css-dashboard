194875b7631c50f32c4bceb290f81f60de080634
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-06-04 14:53:22 -0400

    basic js functionality now wokrs.

--- script.js
+++ script.js
@@ -11,16 +11,19 @@ submit.addEventListener('click', (e) => {
     let title = document.getElementById("task-title").value;
     let details = document.getElementById("task-details").value;
     var cards = document.querySelectorAll('.project-card');
+    let completed = false
     cards.forEach(function(card, index){
-        if (index === 0) return;
-        let innerTitle = card.getElementsByClassName("card-title")
-        console.log(innerTitle[0].innerHTML)
-        let innerDetails = card.getElementsByClassName("card-text")
-        console.log(innerDetails[0].innerHTML)
-        // the below doesn't work, Inner Text returns undefined
-        if (innerDetails[0].innerHTML === 'Nothing here yet...') {
-            innerTitle[0].innerHTML = title;
-            innerDetails[0].innerHTML = details;
+        if(completed === false){
+            if (index === 0) return;
+            let innerTitle = card.getElementsByClassName("card-title")
+            console.log(innerTitle[0].innerHTML)
+            let innerDetails = card.getElementsByClassName("card-text")
+            console.log(innerDetails[0].innerHTML)
+            if (innerDetails[0].innerHTML === 'Nothing here yet...') {
+                innerTitle[0].innerHTML = title;
+                innerDetails[0].innerHTML = details;
+                completed = true;
+                }
             }
     })  
 })
