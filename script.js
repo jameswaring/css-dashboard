151f7bf4fa560f7549d7b6a81a2c97732e4a6a07
Jamie <jamie@pop-os.localdomain>
2023-06-05 07:18:41 -0500

    Rewrote method for adding card, and implemented a delete button

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-let final = document.getElementById("first-card");
-let newCard = document.getElementById("new-card");
-final.addEventListener('click', (e) => {
-    document.getElementById("add-msg").innerHTML = "";
-    newCard.style.display = "flex";
-})
+let final = document.getElementById('first-card');
+let newCard = document.getElementById('new-card');
+let projects = document.getElementById('projects');
+let taskTitle = document.getElementById('task-title');
+let taskDetails = document.getElementById('task-details');
 
-let submit = document.getElementById("task-btn");
-submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    let title = document.getElementById("task-title").value;
-    let details = document.getElementById("task-details").value;
-    var cards = document.querySelectorAll('.project-card');
-    let completed = false
-    cards.forEach(function(card, index){
-        if(completed === false){
-            if (index === 0) return;
-            let innerTitle = card.getElementsByClassName("card-title")
-            console.log(innerTitle[0].innerHTML)
-            let innerDetails = card.getElementsByClassName("card-text")
-            console.log(innerDetails[0].innerHTML)
-            if (innerDetails[0].innerHTML === 'Nothing here yet...') {
-                innerTitle[0].innerHTML = title;
-                innerDetails[0].innerHTML = details;
-                completed = true;
-                }
-            }
-    })  
-})
+final.addEventListener('click', (e) => {
+  document.getElementById('add-msg').innerHTML = '';
+  newCard.style.display = 'flex';
+});
+
+newCard.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let [title, text] = e.target;
+
+  const id = Math.random();
+
+  const card = document.createElement('div');
+  card.className = 'project-card';
+  card.innerHTML = `
+      <div class="card-title">${title.value}</div>
+      <div class="card-text">${text.value}</div>
+      <div class="card-icons">
+        <img src="images/share.svg" width="20px" height="20px">
+      </div>
+      <button class="button" id="${id}">Delete</button>
+  `;
+  projects.appendChild(card);
+
+  const button = document.getElementById(id);
+  button.addEventListener('click', (e) => button.parentElement.remove());
+
+  taskTitle.value = '';
+  taskDetails.value = '';
+});
